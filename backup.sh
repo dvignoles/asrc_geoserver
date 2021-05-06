0db7b04f9b56439c18921a19e4e400ebfe443b99
Daniel Vignoles <dvignoles@gmail.com>
2021-05-06 20:32:15 +0000

    general reorganization and script improvements

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # https://docs.docker.com/storage/volumes/#restore-container-from-backup
-docker run --rm --volumes-from geoserver_geoserver_1 -v /asrc/ecr/danielv/geoserver:/backup ubuntu tar cvf /backup/geoserver_data-dir.tar /opt/geoserver/data_dir
+CONTAINER="${1}"
+TARNAME="${2}"
+docker run --rm --volumes-from ${CONTAINER} -v /asrc/ecr/danielv/geoserver_volumes/backups:/backup ubuntu tar cvf "/backup/${TARNAME}" /opt/geoserver/data_dir
--- sql/db_setup.sql
+++ sql/db_setup.sql
@@ -2,10 +2,17 @@
 -- SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ghaas';
 -- drop database ghaas;
 -- create database ghaas;
-SET search_path = brazil, public;
+-- SET search_path = brazil, public;
 
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+-- Scope geoserver user
+CREATE SCHEMA IF NOT EXISTS geoserver;
+GRANT ALL PRIVILEGES ON SCHEMA geoserver TO geoserver;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA geoserver TO geoserver;
+ALTER DEFAULT PRIVILEGES IN SCHEMA geoserver GRANT ALL PRIVILEGES ON TABLES TO geoserver;
+
 
 do
 $$
@@ -19,11 +26,17 @@ $$
             LOOP
                 -- EXECUTE format('DROP SCHEMA IF EXISTS %I CASCADE', x);
                 EXECUTE format('CREATE SCHEMA IF NOT EXISTS %I', x);
+                EXECUTE format('GRANT USAGE ON SCHEMA %I to geoserver', x);
+                EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA %I TO geoserver', x);
+                EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA %I GRANT SELECT ON TABLES TO geoserver', x);
             end loop;
     end;
 $$;
 
 -- zonalmean, min, max
+-- types for table storage combining annual/monthly data
+-- unpacked into yearly views
+
 CREATE TYPE public.zonaloutput
     as (zmean real, zmin real, zmax real);
 
